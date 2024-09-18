9f3df64dc4b4c942801dc283e81e7c95ba466aa3
Aditya <adityarathore1122@gmail.com>
2024-09-18 12:02:18 +0530

    modified user_role controller

--- app/controllers/user_roles_controller.rb
+++ app/controllers/user_roles_controller.rb
@@ -1,4 +1,6 @@
 class UserRolesController < ApplicationController
+  before_action :find_role, only: %i[update destroy ]
+
   def create
     role = UserRole.new(user_role_params)
     if role.save
@@ -9,17 +11,29 @@ class UserRolesController < ApplicationController
   end
 
   def update
-    role = UserRole.find(params[:id])
-    if role.update(user_role_params)
-      render json: role , status: :updated, location: role
+    if @role.update(user_role_params)
+      render json: @role 
     else
-      render json: role.errors, status: :unprocessable_entity
+      render json: @role.errors, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    if @role.destroy
+      render json: @role , status: 200
+    else
+      render json: @role.errors, status: :unprocessable_entity
+    end
+  end
+
 
   private
 
   def user_role_params
     params.permit(:role , :user_id)
+  end
+
+  def find_role
+    @role = UserRole.find(params[:id])
   end
 end
